b151196ccdef5262491ab54560f5c08bb88bcee6
Triktron <ele.trik@hotmail.com>
2019-05-06 11:22:32 +0200

    redid the download manager and working on a tile view of the comics.

---
+++ src/js/download.js
@@ -0,0 +1,75 @@
+const mkdirp = require('mkdirp');
+const fs = require('fs');
+const _path = require('path');
+
+class DownloadManager {
+  constructor(update_progress) {
+    this.download_queue = [];
+    this.is_running = false;
+
+    this.total = 0;
+    this.done = 0;
+    this.done_timeout = null;
+    this.update_progress = update_progress;
+  }
+
+  download(url, path) {
+    if (fs.existsSync(path)) return;
+
+    this.total++;
+    this.download_queue.push({
+      url: url,
+      path: path
+    })
+
+    if (!this.is_running) this.start();
+  }
+
+  progress() {
+    return this.total == 0 ? 0 : this.done / this.total;
+  }
+
+  start() {
+    var self = this;
+    if (this.is_running) return;
+    this.is_running = true;
+
+    if (this.download_queue.length == 0) return this.is_running = false;
+
+    // make sure the directory exists
+    var current = this.download_queue.shift();
+    console.log(current);
+    mkdirp.sync(_path.dirname(current.path));
+
+    // generate a tmp file in case the download fails
+    var tmpPath = tmp.tmpNameSync();
+    var file = fs.createWriteStream(tmpPath);
+
+    // download the file to the tmp file
+    var start_time = Date.now();
+    got.stream(current.url).pipe(file);
+    file.on('finish', function() {
+      file.close(() => {
+        // move the tmp file to it's place
+        fs.rename(tmpPath, current.path, function (err) {
+          if (err) return console.log(err);
+          self.done++;
+          self.update_progress();
+          if (self.done_timeout) clearTimeout(self.done_timeout);
+          self.done_timeout = setTimeout(() => {
+            self.done = 0;
+            self.total = 0;
+            self.update_progress();
+          },1200)
+
+          // make sure it's been 500 ms since last call to not hit the api limit
+          var time_past = Date.now() - start_time;
+          self.is_running = false;
+          if (time_past > 510) self.start(); else setTimeout(() => self.start(), 510 - time_past);
+        })
+      });
+    });
+  }
+}
+
+module.exports = DownloadManager;
--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,8 @@ const got = require('got');
 const Bottleneck = require("bottleneck/es5");
 const fs = require('fs');
 const { DownloaderHelper } = require('node-downloader-helper');
+var tmp = require('tmp');
+var mkdirp = require('mkdirp');
 
 const config_folder = electron.remote.app.getPath("appData") + "\\e621-comic-reader";
 
@@ -38,37 +40,41 @@ function add_comic(id) {
 }
 
 // update comic data
-function update_comic(id) {
+function update_comic(id, p = 1) {
   var comic = comics[id];
+  var pages = [];
 
-  got("https://e621.net/pool/show/" + id + ".json").then((r) => JSON.parse(r.body)).then(data => {
-    comic.name = data.name;
-    comic.pages = data.posts.map(post => {
-      return {
-        description: post.description,
-        file_url: post.file_url,
-        preview_url: post.preview_url,
-        sources: post.sources,
-        id:post.id
-      }
-    })
+  got("https://e621.net/pool/show/" + id + ".json?page=" + p).then((r) => JSON.parse(r.body)).then(data => {
+      pages = data.posts.map(post => {
+        return {
+          description: post.description,
+          file_url: post.file_url,
+          preview_url: post.preview_url,
+          sources: post.sources,
+          id:post.id
+        }
+      })
+      comic.name = data.name;
+      if (p == 1) comic.pages = pages; else comic.pages = comic.pages.concat(pages);
+      p++;
 
     store.set("comics", comics);
+
+    if (pages.length > 0) setTimeout(() => update_comic(id, p),500); else download_comic(id);
   })
+
 }
 
+const DownloadManager = require('./js/download');
+var dm = new DownloadManager(() => {
+  document.querySelector(".chrome-tabs-bottom-bar.progress").style.display = dm.progress() == 0 ? "none" : "block";
+  document.querySelector(".chrome-tabs-bottom-bar.progress").style.width = dm.progress() * 100 + "%";
+})
+
 function download_comic(id) {
-  var comic = comics[id];
-
-
-  comic.pages.forEach((page) => {
-    if (!fs.existsSync(config_folder + "\\comics\\" + id + "\\" + page.id)) {
-
-      limiter.schedule(() => new Promise(function(resolve, reject) {
-        console.log(page.file_url);
-        resolve();
-      }))
-    }
+  comics[id].pages.forEach((page) => {
+    var path = config_folder + "\\comics\\" + id + "\\" + page.id + "." + page.file_url.split(".").pop();
+    dm.download(page.file_url, path);
   })
 }
 
